1550cf486323e5cf5c4a04ea0ac5b46daad7e901
TgJe <51323906+tangjie133@users.noreply.github.com>
2020-03-20 10:23:25 +0800

    更新 pxt.json, microIoT.ts

--- microIoT.ts
+++ microIoT.ts
@@ -266,7 +266,7 @@ namespace microIoT {
             buf[0] = 0x15;
         }
         buf[1] = angle;
-        pins.i2cWriteBuffer(0x16, buf);
+        pins.i2cWriteBuffer(0x10, buf);
     }
 
     //% weight=49
@@ -294,13 +294,13 @@ namespace microIoT {
                 buf[1] = 0x00;
             }
             buf[2] = speed;
-            pins.i2cWriteBuffer(IIC_ADDRESS, buf);
+            pins.i2cWriteBuffer(0x10, buf);
             buf[0] = 0x02;
             buf[1] = direction;
         } else {
         }
         buf[2] = speed;
-        pins.i2cWriteBuffer(IIC_ADDRESS, buf);
+        pins.i2cWriteBuffer(0x10, buf);
     }
     //% weight=48
     //% blockId=microIoT_motorStop block="Motor stop|%motors"
@@ -315,12 +315,12 @@ namespace microIoT {
             buf[0] = 0x00;
             buf[1] = 0;
             buf[2] = 0;
-            pins.i2cWriteBuffer(IIC_ADDRESS, buf);
+            pins.i2cWriteBuffer(0x10, buf);
             buf[0] = 0x02;
         }
         buf[1] = 0;
         buf[2] = 0;
-        pins.i2cWriteBuffer(IIC_ADDRESS, buf);
+        pins.i2cWriteBuffer(0x10, buf);
     }
 
     /*
